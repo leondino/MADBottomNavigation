8c26233303a1230086ee756592723cb64bc4d2b3
Léon Imanuel <leondino0309@gmail.com>
2020-05-30 13:25:14 +0200

    Initial commit, but already at the final steps

---
+++ app/src/main/java/com/example/bottomnavigation/RateFragment.kt
@@ -0,0 +1,34 @@
+package com.example.bottomnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import kotlinx.android.synthetic.main.fragment_rate.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rate, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnRate.setOnClickListener {
+            val action =
+                RateFragmentDirections.actionRateFragmentToRatedFragment(rbRate.rating)
+            findNavController().navigate(action)
+        }
+    }
+
+}
